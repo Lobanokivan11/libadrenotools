317f4491dbada65e4bbfb75450e213407b74b0d9
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-04-29 16:27:27 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+mkdir -p prebuilt/lib
+mkdir -p prebuilt/include
+
+cp libadrenotools.a prebuilt/lib
+cp lib/linkernsbypass/liblinkernsbypass.a prebuilt/lib
+cp src/hook/*.so prebuilt/lib
+cp -rf ../include/* prebuilt/include
+cp ../lib/linkernsbypass/android_linker_ns.h prebuilt/include
