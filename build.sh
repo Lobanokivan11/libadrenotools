df8f1465dee23f1fbc66f6d7ed48efbc1ac27f4c
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-04-29 16:44:17 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cmake -S . -B ./build -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=28
+cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=28
 cd build
 make
 cd ..
