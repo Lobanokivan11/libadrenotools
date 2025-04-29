1a4bd8a09ec260a56baa367bcc5f2adc3621991f
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-04-29 16:29:27 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+cmake -S . -B ./build \
+    -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \ 
+    -DANDROID_ABI=arm64-v8a \
+    -DANDROID_NATIVE_API_LEVEL=$MINSDKVERSION
 mkdir -p prebuilt/lib
 mkdir -p prebuilt/include
 cp libadrenotools.a prebuilt/lib
