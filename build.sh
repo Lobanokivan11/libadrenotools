d064c64abf0af33c81af70b6b281e4e2d092bed9
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-04-29 16:31:58 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 cmake -S . -B ./build \
     -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \ 
     -DANDROID_ABI=arm64-v8a \
-    -DANDROID_NATIVE_API_LEVEL=$MINSDKVERSION
-mkdir -p prebuilt/lib
-mkdir -p prebuilt/include
-cp libadrenotools.a prebuilt/lib
-cp lib/linkernsbypass/liblinkernsbypass.a prebuilt/lib
-cp src/hook/*.so prebuilt/lib
-cp -rf ../include/* prebuilt/include
-cp ../lib/linkernsbypass/android_linker_ns.h prebuilt/include
+    -DANDROID_NATIVE_API_LEVEL=28
+cd build
+make
+mkdir -p ../prebuilt/lib
+mkdir -p ../prebuilt/include
+cp libadrenotools.a ../prebuilt/lib
+cp lib/linkernsbypass/liblinkernsbypass.a ../prebuilt/lib
+cp src/hook/*.so ../prebuilt/lib
+cp -rf ../include/* ../prebuilt/include
+cp ../lib/linkernsbypass/android_linker_ns.h ../prebuilt/include
+zip -r ../prebuilt.zip ../prebuilt/*
